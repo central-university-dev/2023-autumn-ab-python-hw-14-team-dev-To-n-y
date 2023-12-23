ff13820c90bc99f41f13f8c187efa38da18d9c51
Tony <realvitl@yandex.ru>
2023-12-23 21:22:41 +0300

    [blog-dev] add db models

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
+class Config:
+    POSTGRES_URL = os.environ.get("POSTGRES_URL")
+
+
+config = Config()
